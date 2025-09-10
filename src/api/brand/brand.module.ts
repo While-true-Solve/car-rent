f2f1ea297a33c75db9af95a3407df6ba76ed9032
Javohir-042 <javohirquromboyev933@gmail.com>
2025-09-10 21:09:59 +0500

    conflict hal bo'ldi

--- src/api/brand/brand.module.ts
+++ src/api/brand/brand.module.ts
@@ -2,18 +2,12 @@ import { Module } from '@nestjs/common';
 import { BrandService } from './brand.service';
 import { BrandController } from './brand.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-<<<<<<< HEAD
-import { Brand } from 'src/core';
 
-@Module({
-  imports: [TypeOrmModule.forFeature([Brand])],
-=======
 import { Brand, Car } from 'src/core';
 import { CarModule } from '../car/car.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Brand]), CarModule],
->>>>>>> origin/dev
   controllers: [BrandController],
   providers: [BrandService],
   exports: [BrandService],
